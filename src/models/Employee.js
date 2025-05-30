bf3a181f01480d1604a45b3ce482f04f2532be6c
srivastavarahul442 <srivastavarahul442@gmail.com>
2025-05-30 13:55:02 +0530

    senitaized data

--- src/models/Employee.js
+++ src/models/Employee.js
@@ -1,16 +1,33 @@
-// models/Employee.js
 import mongoose from "mongoose";
-import User from "./User.js"; // import base model
+import User from "./User.js"; // base model
 
 const employeeSchema = new mongoose.Schema({
-  joiningDate: { type: Date  },
-  departmentId: { type: String },
-  managerId: { type: String },
-  designation: { type: String },
-  
+  joiningDate: {
+    type: Date, // No validator, just a plain date
+  },
+  departmentId: {
+    type: String,
+    trim: true,
+    minlength: [2, "Department ID must be at least 2 characters long"],
+    required: [true, "Department ID is required"],
+  },
+  managerId: {
+    type: String,
+    trim: true,
+    minlength: [2, "Manager ID must be at least 2 characters long"],
+    required: [true, "Manager ID is required"],
+  },
+  designation: {
+    type: String,
+    trim: true,
+    minlength: [2, "Designation must be at least 2 characters long"],
+    maxlength: [50, "Designation must be at most 50 characters long"],
+    required: [true, "Designation is required"],
+  },
 });
 
 // Extend User using discriminator
-const Employee = mongoose.models.Employee || User.discriminator("Employee", employeeSchema);
+const Employee =
+  mongoose.models.Employee || User.discriminator("Employee", employeeSchema);
 
 export default Employee;
