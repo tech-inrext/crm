9ebaeeb6fd87a0b9e407cd6a31fc1ffb3829a553
srivastavarahul442 <srivastavarahul442@gmail.com>
2025-06-13 18:50:45 +0530

    created schema & apis for department and Added nodemailer configuration for sending emails to lead via SMTP

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -4,7 +4,7 @@ import Employee from "../models/Employee";
 import dbConnect from "../lib/mongodb";
 import { checkPermission } from "../utils/checkPermission";
 
-const MODULES = ["lead", "employee", "role"];
+const MODULES = ["lead", "employee", "role","department"];
 
 export async function userAuth (req, res, next){
   try {
