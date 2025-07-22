586a1706277329d3a659f0185da1353498166f00
Nandan Kumar <109342663+nandanSrivastava@users.noreply.github.com>
2025-07-22 18:44:43 +0530

    Update Employee.js

--- src/models/Employee.js
+++ src/models/Employee.js
@@ -3,7 +3,7 @@ import validator from "validator";
 
 const employeeSchema = new mongoose.Schema(
   {
-    fullName: {
+    name: {
       type: String,
       required: [true, "Name is required"],
       trim: true,
