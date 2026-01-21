7ad55b69098850b13776665bdf0700337225b00c
Rahul Srivastava <118062330+srivastavarahul442@users.noreply.github.com>
2026-01-21 17:54:34 +0530

    code refectored for cab booking, department, cab vendor (#188)

--- src/pages/api/v0/department/index.js
+++ src/pages/api/v0/department/index.js
@@ -1,67 +1,19 @@
-import dbConnect from "../../../../lib/mongodb";
-import Department from "../../../../models/Department";
-import * as cookie from "cookie";
-import { userAuth } from "../../../../middlewares/auth";
+import { Controller } from "@framework";
+import DepartmentService from "../../../../be/services/DepartmentService";
 
-const createDepartment = async (req, res) => {
-  try {
-    const { name, description, managerId, departmentId } = req.body;
-
-    if (!name) {
-      return res
-        .status(400)
-        .json({ success: false, message: "Department name is required" });
-    }
-
-    const newDept = new Department({
-      name,
-      description,
-      managerId,
-      departmentId,
-    });
-    await newDept.save();
-
-    return res.status(201).json({ success: true, data: newDept });
-  } catch (error) {
-    return res.status(500).json({ success: false, message: error.message });
+class DepartmentIndexController extends Controller {
+  constructor() {
+    super();
+    this.service = new DepartmentService();
   }
-};
 
-const getAllDepartment = async (req, res) => {
-  try {
-    const departments = await Department.find({ isActive: true }).populate(
-      "managerId",
-      "name email"
-    );
-    return res.status(200).json({ success: true, data: departments });
-  } catch (error) {
-    return res.status(500).json({ success: false, message: error.message });
+  async get(req, res) {
+    return this.service.getAllDepartment(req, res);
   }
-};
 
-// Authenticate first
-function withAuth(handler) {
-  return async (req, res) => {
-    const parsedCookies = cookie.parse(req.headers.cookie || "");
-    req.cookies = parsedCookies;
-    await userAuth(req, res, () => handler(req, res));
-  };
+  async post(req, res) {
+    return this.service.createDepartment(req, res);
+  }
 }
 
-const handler = async (req, res) => {
-  await dbConnect();
-
-  if (req.method === "POST") {
-    return createDepartment(req, res);
-  }
-
-  if (req.method === "GET") {
-    return getAllDepartment(req, res);
-  }
-
-  return res
-    .status(405)
-    .json({ success: false, message: "Method not allowed" });
-};
-
-export default withAuth(handler);
+export default new DepartmentIndexController().handler;
