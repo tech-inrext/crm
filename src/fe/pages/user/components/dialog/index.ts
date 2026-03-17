6f0b6fe647c58278510a85c61a19a002986b5d64
Nandan Shrivastava <nandan.shri21@gmail.com>
2026-03-17 14:18:07 +0530

    fix: import issue changed ts to tsx

--- src/fe/pages/user/components/dialog/index.ts
+++
@@ -1,2 +0,0 @@
-export { default as UserDialog } from "./UserDialog";
-export { default as UserDialogView } from "./UserDialogView";
---
+++ src/fe/pages/user/components/dialog/index.tsx
@@ -0,0 +1,2 @@
+export { default as UserDialog } from "./UserDialog/index.tsx";
+export { default as UserDialogView } from "./UserDialogView/index.tsx";
