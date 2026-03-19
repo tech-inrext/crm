a76aceb5bc1f3d2341e7905284acd74414d32274
Nandan Shrivastava <nandan.shri21@gmail.com>
2026-03-19 15:40:39 +0530

    fix: moved constants from multiple files to one

--- src/fe/pages/user/constants/documents.ts
+++
@@ -1,9 +0,0 @@
-import { FIELD_LABELS } from "./users";
-import { DocumentConfig } from "@/fe/pages/user/types/documents";
-
-export const DOCUMENTS: DocumentConfig[] = [
-  { id: "aadhar-upload", fieldName: "aadharFile", labelKey: "AADHAR" },
-  { id: "pan-upload", fieldName: "panFile", labelKey: "PAN" },
-  { id: "bank-upload", fieldName: "bankProofFile", labelKey: "BANK_PROOF" },
-  { id: "sig-upload", fieldName: "signatureFile", labelKey: "SIGNATURE" },
-];
