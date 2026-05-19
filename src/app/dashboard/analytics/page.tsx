604bcf5d5431cf2c9b650c80978a3d5bc318ae77
srivastavarahul442 <srivastavarahul442@gmail.com>
2026-05-19 09:39:26 +0530

    removed the production env

--- src/app/dashboard/analytics/page.tsx
+++ src/app/dashboard/analytics/page.tsx
@@ -3,11 +3,11 @@
 import Analytics from "@/fe/pages/analytics";
 
 export default function AnalyticsPage() {
-  const isProduction = process.env.NEXT_PUBLIC_APP_ENV === "production";
+  // const isProduction = process.env.NEXT_PUBLIC_APP_ENV === "production";
 
-  if (isProduction) {
-    return null;
-  }
+  // if (isProduction) {
+  //   return null;
+  // }
 
   return <Analytics />;
 }
